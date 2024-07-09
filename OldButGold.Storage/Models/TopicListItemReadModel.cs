8b1d051bfb745b0c7e23ae407f0cdb455e2c16ab
Andrey Nazarov <gas4556@mail.ru>
2024-07-09 23:54:50 +0300

    Some fixes

---
+++ OldButGold.Storage/Models/TopicListItemReadModel.cs
@@ -0,0 +1,17 @@
+﻿namespace OldButGold.Forums.Storage.Models
+{
+    internal class TopicListItemReadModel
+    {
+        public Guid TopicId { get; set; }   
+        
+        public string Title { get; set; }
+
+        public DateTimeOffset CreatedAt { get; set; }
+
+        public int TotalCommentsCount { get; set; }
+
+        public DateTimeOffset? LastCommentCreatedAt { get; set; }
+
+        public string CommentText { get; set; }
+    }
+}
--- OldButGold.Storage/Storages/GetTopicsStorage.cs
+++ OldButGold.Storage/Storages/GetTopicsStorage.cs
@@ -2,6 +2,7 @@
 using OldButGold.Forums.Domain.Models;
 using OldButGold.Forums.Domain.UseCases.GetTopics;
 using OldButGold.Forums.Storage;
+using OldButGold.Forums.Storage.Models;
 
 namespace OldButGold.Forums.Storage.Storages;
 
@@ -10,9 +11,37 @@ internal class GetTopicsStorage(
 {
     public async Task<(IEnumerable<Topic> resources, int totalCount)> GetTopics(Guid forumId, int skip, int take, CancellationToken cancellationToken)
     {
+
         var query = dbContext.Topics.Where(t => t.ForumId == forumId);
 
         var totalCount = await query.CountAsync(cancellationToken);
+
+        var topicReadModels = await dbContext.Database.SqlQuery<TopicListItemReadModel>($@"
+            SELECT
+                t.""TopicId"" AS ""TopcId"",
+                t.""Title"" AS ""Title"",
+                t.""CreatedAt"" AS ""CreatedAt"",
+                COALESCE(c.TotalCommentsCount, 0) AS ""TotalCommentsCount"",
+                c.""CreatedAt"" AS ""LastCommentCreatedAt"",
+            	c.""Text""  AS ""CommentText"" 
+            FROM ""Topics"" AS t    
+            LEFT JOIN (
+            	SELECT 
+            		""TopicId"",
+            		""CommentId"",
+            		""CreatedAt"",
+            		""Text"" ,
+            		COUNT(*) OVER (PARTITION BY ""TopicId"") AS TotalCommentsCount,
+            		ROW_NUMBER() OVER(PARTITION BY ""TopicId"" ORDER BY ""CreatedAt"" DESC) AS rn
+            	FROM ""Comments""
+            ) AS c ON t.""TopicId"" = c.""TopicId"" AND c.rn = 1
+            WHERE t.""ForumId"" = {forumId}
+            ORDER BY 
+            	COALESCE(c.""CreatedAt"", t.""CreatedAt"") DESC
+            LIMIT {take}
+            OFFSET {skip};
+        ").ToArrayAsync(cancellationToken);
+
         var resources = await query
             .Select(t => new Topic
             {
@@ -29,3 +58,4 @@ internal class GetTopicsStorage(
         return (resources, totalCount);
     }
 }
+
